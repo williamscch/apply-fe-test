836c3473ccb6ea7b926dff902a39e56b56eb8795
Williams <williamscolmenaresch@gmail.com>
2025-01-03 00:37:46 -0400

    feat:(cart) add cart summary organism

--- src/components/templates/cart/CartTemplate.tsx
+++ src/components/templates/cart/CartTemplate.tsx
@@ -2,6 +2,7 @@ import { ROUTES } from "@/config/routes";
 import { IconName } from "@/components/atoms/Icon";
 import PageHeaderWithLink from "@/components/molecules/PageHeaderWithLink";
 import CartGames from "@/components/organisms/cart/CartGames";
+import CartSummary from "@/components/organisms/cart/CartSummary";
 
 interface CartTemplateProps {
   title?: string;
@@ -20,8 +21,9 @@ export default function CartTemplate({
   return (
     <section className="flex flex-col gap-8 md:gap-12">
       <PageHeaderWithLink title={title} subtitle={subtitle} link={link} />
-      <div className="grid md:grid-cols-4fr_3fr gap-12 md:gap-20">
+      <div className="grid md:grid-cols-4fr_3fr gap-12 lg:gap-20">
         <CartGames />
+        <CartSummary />
       </div>
     </section>
   );
